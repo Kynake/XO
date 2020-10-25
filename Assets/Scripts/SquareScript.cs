57c8f21e6b432776bac0f580e59e39a5a8165df0
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2020-10-25 19:56:18 -0300

    Initial Commit - With LFS

---
+++ Assets/Scripts/SquareScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareScript : MonoBehaviour {
+    public int Line;
+    public int Column;
+
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
