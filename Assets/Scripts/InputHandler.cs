6c4467a620c93d6d055986dc5133d358fdd01065
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2020-11-14 04:17:55 -0300

    Sound on click

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class InputHandler : MonoBehaviour {
+
+    private AudioSource _clickSound;
+
+    void Start() {
+        _clickSound = GetComponent<AudioSource>();
+    }
+
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            _clickSound.Play();
+        }
+    }
+}
