36d4989fa82a3f2f1c6e851f9481cc3981be86f6
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2020-11-15 20:47:32 -0300

    use 2 space on InputHandler

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputHandler : MonoBehaviour {
-    public List<AudioClip> clickSounds = new List<AudioClip>();
-    private AudioSource _clickPlayer;
+  public List<AudioClip> clickSounds = new List<AudioClip>();
+  private AudioSource _clickPlayer;
 
-    void Start() {
-        _clickPlayer = GetComponent<AudioSource>();
-    }
+  void Start() {
+    _clickPlayer = GetComponent<AudioSource>();
+  }
 
-    void Update() {
-        if(Input.GetMouseButtonDown(0)) {
-            if(clickSounds.Capacity > 0) {
-                _clickPlayer.clip = clickSounds[Random.Range(0, clickSounds.Capacity)];
-                _clickPlayer.Play();
-            }
-        }
+  void Update() {
+    if(Input.GetMouseButtonDown(0)) {
+      if(clickSounds.Capacity > 0) {
+        _clickPlayer.clip = clickSounds[Random.Range(0, clickSounds.Capacity)];
+        _clickPlayer.Play();
+      }
     }
+  }
 }
