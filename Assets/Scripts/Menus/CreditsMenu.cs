43acceef8456cbbd0d48d0396cb9499cad8ad5de
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2020-11-15 15:29:35 -0300

    Add credits

---
+++ Assets/Scripts/Menus/CreditsMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsMenu : MonoBehaviour {
+
+  public TextAsset credits;
+
+  private void Awake() {
+    Text creditsDisplay = gameObject.GetComponentInChildren<Text>();
+    creditsDisplay.text = credits.text;
+  }
+
+}
