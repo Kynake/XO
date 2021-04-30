c86337bfe62869451bf55352639757cc5f8d7d50
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-04-30 20:07:41 -0300

    Create NetworkManager object

---
+++ Assets/Scripts/NetworkManagerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using MLAPI.Transports;
+using MLAPI.Transports.UNET;
+using MLAPI.Transports.PhotonRealtime;
+
+public class NetworkManagerController : MonoBehaviour {
+    private NetworkManager _netManager = null;
+    private NetworkTransport _ipTransport = null;
+    private NetworkTransport _relayedTransport = null;
+
+    private NetworkTransport _currentTransport {
+        get {
+            return _netManager?.NetworkConfig.NetworkTransport;
+        }
+        set {
+            if(_netManager != null) {
+                _netManager.NetworkConfig.NetworkTransport = value;
+            }
+        }
+    }
+
+    private void Awake() {
+        _netManager = GetComponent<NetworkManager>();
+        _ipTransport = GetComponent<UNetTransport>();
+        _relayedTransport = GetComponent<PhotonRealtimeTransport>();
+    }
+}
