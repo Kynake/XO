d3538f49c9c7eec881b90b267084d7f2dbfbac9b
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-05-01 02:34:38 -0300

    First work on Player script

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using MLAPI.Messaging;
+using MLAPI.NetworkVariable;
+
+public class PlayerState : NetBehaviour {
+  private NetworkVariable<Symbol> _playerSymbol = new NetworkVariable<Symbol>(new NetworkVariableSettings {
+    WritePermission = NetworkVariablePermission.ServerOnly,
+    ReadPermission = NetworkVariablePermission.Everyone
+  });
+
+  public Symbol playerSymbol {
+    get {
+      return _playerSymbol.Value;
+    }
+    set {
+      if(!IsServer) {
+        return;
+      }
+
+      _playerSymbol.Value = value;
+    }
+  }
+}
