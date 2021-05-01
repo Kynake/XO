d1c87e66d78b6496fc6810c61a42560975311f4d
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-05-01 02:32:47 -0300

    Move Square script out of legacy

---
+++ Assets/Scripts/Square/Square.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour {
+
+
+  private Board _parentBoard;
+  private SquareArt _squareArt;
+
+  private Symbol _currentSymbol;
+  public Symbol currentSymbol {
+    get {
+      return _currentSymbol;
+    }
+    set {
+      if(_currentSymbol == value) {
+        return;
+      }
+      _currentSymbol = value;
+      _squareArt.currentSymbol = currentSymbol;
+    }
+  }
+
+  private void Awake() {
+    _parentBoard = GetComponentInParent<Board>();
+    _squareArt = GetComponentInChildren<SquareArt>();
+  }
+
+  private void Start() {
+    currentSymbol = Symbol.None;
+  }
+
+  private void OnMouseDown() {
+    print("Click");
+
+    // TODO make this into a delegate that the board subscribes to
+    // Run Board function
+    _parentBoard.cycleSymbol(this);
+
+  }
+}
